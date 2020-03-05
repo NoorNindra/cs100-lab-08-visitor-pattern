c16975220a665e49b47fd13bf23661093914fee2
Isis Dumas <isis.dumas000@gmail.com>
2020-03-05 11:33:25 -0800

    Added code to visitor.hpp

--- visitor.hpp
+++ visitor.hpp
@@ -18,33 +18,47 @@ class CountVisitor{
         int paren;
 
     public:
-        CountVisitor();
+        CountVisitor() {
+             ops = 0;
+             rands = 0;
+             mults = 0;
+             divs = 0;
+             add = 0;
+             sub = 0;
+             pow = 0;
 
-        void visit_op();
-        int op_count();
-        void visit_rand();
-        int rand_count();
-        void visit_mult();
-        int mult_count();
-        void visit_div();
-        int div_count();
-        void visit_add();
-        int add_count();
-        void visit_sub();
-        int sub_count();
-        void visit_pow();
-        int pow_count();
+             ceil = 0;
+             floor = 0;
+             abs = 0;
+             trunc = 0;
+             paren = 0;
+        }
 
-        void visit_ceil();
-        int ceil_count();
-        void visit_floor();
-        int floor_count();
-        void visit_abs();
-        int abs_count();
-        void visit_trunc();
-        int trunc_count();
-        void visit_paren();
-        int paren_count();
+        void visit_op() { ++ops; }
+        int op_count() { return ops; }
+        void visit_rand() { ++rands; }
+        int rand_count() { return rands; }
+        void visit_mult() { ++mults; }
+        int mult_count() { return mults; }
+        void visit_div() { ++divs; }
+        int div_count() { return divs; }
+        void visit_add() { ++add; }
+        int add_count() { return add; }
+        void visit_sub() { ++sub; }
+        int sub_count() { return sub; }
+        void visit_pow() { ++pow; }
+        int pow_count() { return pow; }
+
+        void visit_ceil() { ++ceil; }
+        int ceil_count() { return ceil; }
+        void visit_floor() { ++floor; }
+        int floor_count() { return floor; }
+        void visit_abs() { ++abs; }
+        int abs_count() { return abs; }
+        void visit_trunc() { ++trunc; }
+        int trunc_count() { return trunc; }
+        void visit_paren() { ++paren; }
+        int paren_count() { return paren; }
 };
 
 #endif //__COUNT_VISITOR_HPP__
