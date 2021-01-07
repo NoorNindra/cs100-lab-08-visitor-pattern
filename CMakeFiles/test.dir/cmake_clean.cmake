5af690f2e0c2b25ef51c39f1c140f65d61be0c3b
ashnachadha <ashnachadha@gmail.com>
2021-01-06 19:16:27 -0800

    add lab 8 files

---
+++ CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/binary_iterator.cpp.o"
+  "CMakeFiles/test.dir/preorder_iterator.cpp.o"
+  "CMakeFiles/test.dir/unary_iterator.cpp.o"
+  "CMakeFiles/test.dir/unit_test.cpp.o"
+  "test.pdb"
+  "test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
